6208a243164c4646cf193e097bc0c241040d52a5
vsthedesigner <vs@vsthedesigner.com>
2015-06-19 09:50:28 -0700

    2015-06-19
    -Added JS to format values pulled in from the JSON file

--- js/api.js
+++ js/api.js
@@ -7,22 +7,27 @@
       var lowerProgramName ='.program-panel1 > div > .row > div > .lower-program-name' + i;
       var totalMonthlySales = '.program-panel' + i + '> div > .panel-section-info.column2 > span';
       var lowerMonthlySales = '.program-panel1 > div > .row > div > div.lower-monthly-sales' + i + '> span';
-      var monthlyAttendance = '.program-panel1.last-panel > div > .row > div > div.monthly-attendance' +i;
+      var monthlyAttendance = '.program-panel1.last-panel > div > .row > div > span.monthly-attendance' +i;
       // replace/insert data in corresponding div
       $(programName).text(data.Name);
       $(lowerProgramName).text(data.Name);
-      $(totalMonthlySales).text(data.TotalMonthlySales);
-      $(lowerMonthlySales).text(data.TotalMonthlySales);
+      var totalMonthlySalesFormatted = numeral(data.TotalMonthlySales).format('0,0');
+      $(totalMonthlySales).text(totalMonthlySalesFormatted);
+
+      $(lowerMonthlySales).text(totalMonthlySalesFormatted);
       $(monthlyAttendance).text(data.MonthlyAttendance);
       });
     });
+
+
+// '1,000'
   // Populate panel info
   // $.get ('https://api.myjson.com/bins/17oy7', function(salesArray) {
        //loop through data with a count of i and inject data in corresponding list element
   //   salesArray.forEach(function(sdata,j) {
   //     var planName = '.program-panel' + j + '> div > div.info-panel > div.panel-section-info.column1 > ul > li:nth-child('+ j +')';
   //     var planSales= '.program-panel' + j + '> div > div.info-panel > div.panel-section-info.column2 > ul > li:nth-child('+ j +')';
-         // insert/replace text in li element      
+         // insert/replace text in li element
   //     $(planName).text(sdata.Name)
   //     $(planSales).text(sdata.Sales)
   //   });
